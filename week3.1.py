d98796e0268363e585d18e06e9271b86789db6c0
21350010 <32065584+21350010@users.noreply.github.com>
2017-10-17 10:36:10 +0100

    Program for adding 2 numbers
    Python Data Type-Integer(Variable).

---
+++ week3.1.py
@@ -0,0 +1,8 @@
+#THIS PROGRAM FOR ADDING TWO NUMBERS
+num1=int(input("pls enter number one"))
+print("Thank you")
+num2=int(input("pls enter number two"))
+print("Thank you")
+result=num1+num2
+print("Answer is")
+print(result);
